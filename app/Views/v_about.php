3a3795bff40154982ceeb83fc69e93ad76144608
Vira007 <viraamaghvira@gmail.com>
2021-09-16 20:44:08 +0700

    first commit

---
+++ app/Views/v_about.php
@@ -0,0 +1,7 @@
+<div class="container mt-3">
+    <div class="text-center">
+        <img src="/assets/images/rqs.jpg" alt="logo" width="180" class="img-thumbnail rounded-circle" style="border:5px solid Lavender;">
+        <h1 class="display-4">Praktikum Web Lanjut Dengan Studi Kasus <br> Blog App</h1>
+        <p class="lead">Programming | CodeIgniter 4 | PHP</p>
+    </div>
+</div>
---
+++ app/Views/v_home.php
@@ -0,0 +1,4 @@
+<div class="container mt-3">
+    <h1 class="text-center">Re:Hello World!</h1>
+    <h2 class="text-center">Selamat Datang di Praktikum Web Lanjut</h2>
+</div>
